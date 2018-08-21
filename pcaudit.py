263b233de3550c34d3b49775c1c7e0872ab105d2
Brian Metzger <metzgerb@oregonstate.edu>
2018-08-21 11:29:43 -0500

    added requirements file and cpu details
    added requirements file for psutil
    added cpu details in sysinfo

--- pcaudit.py
+++ pcaudit.py
@@ -9,13 +9,18 @@ import sysinfo
 #TODO: Check arguments for commandline usage
 
 #TODO: Determine Operating system
-sys_platform = sysinfo.get_system_info()
-for x in sys_platform:
-    print(x + ":\t" + sys_platform[x])
 
 
 #TODO: Get system info
-
+platform_info = sysinfo.get_system_info()
+for x in platform_info:
+    print(x + ":\t" + platform_info[x])
+    
+cpu_info = sysinfo.get_cpu_info()
+for x in cpu_info:
+    print(x + ":\t" + str(cpu_info[x]))
+    
+    
 #TODO: Get installed program info
 
 #TODO: Write data to file
--- sysinfo.py
+++ sysinfo.py
@@ -5,11 +5,19 @@ Last modified: 2018-08-21
 """
 
 import platform
+import psutil
 
 def get_system_info():
     sys_dict = {}
+    sys_dict["PC Name"] = platform.node()
     sys_dict["OS"] = platform.platform()
-    sys_dict["Architecture"] = platform.architecture()[0]
-    sys_dict["Processor Type"] = platform.processor()
-    sys_dict["Computer Name"] = platform.node()
+    sys_dict["Arch"] = platform.architecture()[0]
     return sys_dict
+    
+def get_cpu_info():
+    cpu_dict = {}
+    cpu_dict["CPU Type"] = platform.processor()
+    cpu_dict["Freq. (Mhz)"] = psutil.cpu_freq()[2]
+    cpu_dict["Physical Cores"] = psutil.cpu_count(logical=False)
+    cpu_dict["Logical Cores"] = psutil.cpu_count()
+    return cpu_dict
